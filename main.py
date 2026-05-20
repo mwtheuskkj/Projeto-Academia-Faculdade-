f32aec5a70abca2255f0c0e17c7953d6b14fcb45
Matheus Antunes <matheus.antunes2908@gmail.com>
2026-05-19 22:16:51 -0300

    matheus update 3.0
    reajuste dos problemas de o menu não funcionar quando era solicitado tal opção

--- main.py
+++ main.py
@@ -1,47 +1,58 @@
-#organizando o main.py ainda
+# menu principal para rodar tudo do tarefas.py
 
+import os
 from colorama import Style, Fore, Back, init
-init(autoreset=True)
-
 import tarefas
 import utils
 
+init(autoreset=True)
+
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def menu_principal():
-    print(Style.BRIGHT + '=' * 40)
-    print(Style.BRIGHT + 'MENU PRINCIPAL ACADEMIA'.center(40))
-    print(Style.BRIGHT + '=' * 40)
-    print('- 1 Cadastrar Aluno(a)')
-    print('- 2 Listar Alunos')
-    print('- 3 Fila de Atendimento')
-    print('- 4 Atender Próximo Aluno')
-    print('- 5 Check-in de Treino')
-    print('- 6 Histórico de Treinos')
-    print(Style.BRIGHT + Fore.RED + '- 0 Encerrar ')
-    print(Style.BRIGHT + '=' * 40)
-
-def main():
     while True:
-        menu_principal()
-        opcoes = int(input('Escolha uma opção (1-6): '))
-
-        match opcoes:
+        limpar_tela()
+        utils.exibir_menu('ACADEMIA - MENU PRINCIPAL')
+        
+        print(Style.BRIGHT + "1 - Cadastrar aluno(a)")
+        print(Style.BRIGHT + "2 - Lista de alunos ativos")
+        print(Style.BRIGHT + "3 - Entrar na fila de atendimento")
+        print(Style.BRIGHT + "4 - Atender Próximo Aluno")
+        print(Style.BRIGHT + "5 - Registrar check-in de treino")
+        print(Style.BRIGHT + "6 - Visualizar histórico de treinos")
+        print(Style.BRIGHT + Fore.RED + "0 - Encerrar programa")
+        print(Style.BRIGHT + '=' * 40)
+        
+        opcao = int(input(Style.BRIGHT + "Escolha uma opção (1-6): "))
+        
+        # match para escolher alguma opção do menu.
+        match opcao:
             case 1:
+                limpar_tela()
                 tarefas.cadastrar_aluno()
             case 2:
+                limpar_tela()
                 tarefas.listar_alunos()
             case 3:
+                limpar_tela()
                 tarefas.entrar_fila()
             case 4:
+                limpar_tela()
                 tarefas.atender_aluno()
             case 5:
+                limpar_tela()
                 tarefas.registrar_treino()
             case 6:
+                limpar_tela()
                 tarefas.visualizar_historico()
             case 0:
-                print(Style.BRIGHT + Fore.RED + 'Encerrando Sistema...')
-
+                limpar_tela()
+                print(Style.BRIGHT + Fore.RED + '\nEncerrando programa...')
+                break
             case _:
-                print(Style.BRIGHT + Fore.RED + 'Opção Inválida, Tente Novamente!')
-    
-if __name__ == '__main__':
-    main()
+                print(Style.BRIGHT + Fore.RED + '\nOpção inválida! Tente novamente...')
+                input(Style.BRIGHT + '\nPressione ENTER para confirmar...')
+
+if __name__ == "__main__":
+    menu_principal()
